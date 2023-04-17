95bdcfce4f6e72efc4cb777ac62e71ffd700baa8
MarcosYH <marcosdeimos18@gmail.com>
2023-04-17 23:48:45 +0200

    Updates

---
+++ precache-manifest.a353fd5ed104e992e439230db5c7a47d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1f5ff5998a1e15698dc",
+    "url": "/Shop/static/css/main.8782772b.chunk.css"
+  },
+  {
+    "revision": "a1f5ff5998a1e15698dc",
+    "url": "/Shop/static/js/main.a1f5ff59.chunk.js"
+  },
+  {
+    "revision": "de777e5fde807a0452ce",
+    "url": "/Shop/static/js/1.de777e5f.chunk.js"
+  },
+  {
+    "revision": "b378cbc4873d60cc2c89",
+    "url": "/Shop/static/css/2.d1d08e67.chunk.css"
+  },
+  {
+    "revision": "b378cbc4873d60cc2c89",
+    "url": "/Shop/static/js/2.b378cbc4.chunk.js"
+  },
+  {
+    "revision": "1265ae6c8457afcae7d6",
+    "url": "/Shop/static/js/runtime~main.1265ae6c.js"
+  },
+  {
+    "revision": "5be7f2e86c75a305bce60f39e28fcc45",
+    "url": "/Shop/static/media/Fancy.5be7f2e8.jpg"
+  },
+  {
+    "revision": "481801ccca58e979b57dce407b219f8a",
+    "url": "/Shop/static/media/Fancy Product1.481801cc.jpg"
+  },
+  {
+    "revision": "14ee3a01399f202cd75467c083b093bf",
+    "url": "/Shop/static/media/Popular Item.14ee3a01.jpg"
+  },
+  {
+    "revision": "f1a27e2b210bd7b67b30f56c6a555dcb",
+    "url": "/Shop/static/media/Special Item.f1a27e2b.jpg"
+  },
+  {
+    "revision": "4cca055e03c6b6edc15b97e3d5ebab72",
+    "url": "/Shop/static/media/mannequin3.4cca055e.png"
+  },
+  {
+    "revision": "6e3ac16363344d7f4ad6f16f8631acef",
+    "url": "/Shop/static/media/Popular Item1.6e3ac163.jpg"
+  },
+  {
+    "revision": "d7b152409cc764fd334146f4a1a9a56d",
+    "url": "/Shop/static/media/Sale Item1.d7b15240.jpg"
+  },
+  {
+    "revision": "f075832417a11c19b4a870244f41d51b",
+    "url": "/Shop/static/media/Special Item1.f0758324.jpg"
+  },
+  {
+    "revision": "1b2737a8cf1626141981dbe6aab0d49c",
+    "url": "/Shop/static/media/Jackets.1b2737a8.jpg"
+  },
+  {
+    "revision": "f2e01bac9e9517c93ba2915e678ae16d",
+    "url": "/Shop/static/media/mannequin.f2e01bac.png"
+  },
+  {
+    "revision": "5b2f21ecaf5d1cb7ee15a5326902e87e",
+    "url": "/Shop/index.html"
+  }
+];
